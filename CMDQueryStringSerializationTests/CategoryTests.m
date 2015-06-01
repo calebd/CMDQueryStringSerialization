7fbb659faa0b12f7e3b3675c48ba70e56358db46
Sam Soffes <sam@soff.es>
2015-06-01 09:49:12 -0700

    Add failing test for double encode bug

--- CMDQueryStringSerializationTests/CategoryTests.m
+++ CMDQueryStringSerializationTests/CategoryTests.m
@@ -75,4 +75,17 @@
     XCTAssertEqualObjects(URL.absoluteString, [URL cmd_URLByAddingQueryDictionary:@{}].absoluteString);
 }
 
+- (void)testEncodeAndDecode {
+    NSDictionary *parameters = @{
+                                 @"message": @"Hello World",
+                                 @"number": @42,
+                                 @"favorite_things": @[ @"cats", @"flower bed spreads" ],
+                                 @"dictionary": @{ @"stuff": @"things and whatnot" }
+                                 };
+
+    NSURL *URL = [NSURL URLWithString:@"http://apple.com"];
+    URL = [URL cmd_URLByAddingQueryDictionary:parameters];
+    XCTAssertEqualObjects(URL.cmd_queryDictionary, parameters);
+}
+
 @end
